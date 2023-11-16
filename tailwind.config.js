35acfa2bbbcfb75ff77537fcd7c650b7eba5a8a2
Toheeb Opeyemi AJAYI <toheebopeyemi9@gmail.com>
2023-11-16 19:04:26 +0100

    Adding Colors to the Tailwind Config File

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,30 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        arimo: ["arimo", "sans-serif"],
+        intern: ["intern", "sans-serif"],
+        sans: ["Nunito", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        greens: {
+          100: "#008000",
+          200: "#008000E5",
+          300: "#008000CC",
+          400: "#008000B2",
+          500: "#00800099",
+          600: "#00800080",
+          700: "#00800066",
+          800: "#0080004D",
+          900: "#00800033",
+          1000: "#0080001A",
+          1100: "#0080001A"
+        },
+        blues: {
+
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
