6a5d8d65a4b55f5a1ebb1044fcad51769e506b57
Toheeb Opeyemi AJAYI <toheebopeyemi9@gmail.com>
2023-11-19 05:46:43 +0100

    AuthLayout Created

---
+++ app/layouts/AuthLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function AuthLayout({ text, children }) {
+    return (
+        <section className='border-2 border-red-700 min-h-screen px-5 py flex flex-col justify-center'>
+            <div>
+                <h1 className='text-[#272727] text-5xl font-bold font-arimo text-center'>
+                    {text} to Learn and Connet
+                </h1>
+                <div className='w-full max-w-[490] mx-auto px-8 mt-16'>
+                    {children}
+                </div>
+            </div>
+        </section>
+    )
+}
