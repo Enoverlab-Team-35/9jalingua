99fac3e0943cd42bed329532bccb4616c0d0e260
Toheeb Opeyemi AJAYI <toheebopeyemi9@gmail.com>
2023-11-27 20:56:52 +0100

    Cultural Resources page done

---
+++ app/components/cultural-resources/Linguistics.jsx
@@ -0,0 +1,61 @@
+import Image from 'next/image'
+import React from 'react'
+
+const linguistic = [
+    {
+        img: "/cultural-resources/waves.svg",
+        name: "Melodies of Nigeria",
+        description: `Immerse yourself in the soul-stirring sounds of Nigeria. From traditional beats to contemporary rhythms, our audio collection captures the diverse`
+    },
+    {
+        img: "/cultural-resources/waves.svg",
+        name: "Language Tunes",
+        description: `Explore the beauty of Nigerian languages through our curated audio snippets. Hear the cadence, and pronunciation.`
+    },
+    {
+        img: "/cultural-resources/waves.svg",
+        name: "Festival Vibes",
+        description: `Experience the lively atmosphere of Nigerian festivals with our audio captures. From the beats of drums to the cheers of the crowd.`
+    },
+]
+
+export default function Linguistics() {
+    return (
+        <section className='mt-24 font-arimo px-5'>
+            <div className="container mx-auto">
+                <div className='w-full max-w-[710px] mx-auto text-center'>
+                    <h1 className='text-blues-1100 font-bold text-5xl leading-normal'>
+                        Linguistics Beats
+                    </h1>
+                    <p className='mt-6 text-grays-800 text-xl leading-normal'>
+                        Explore Nigeria's vibrant soundscape, a cultural journey through language, rhythm, and heritage in each curated track
+                    </p>
+                </div>
+
+                <div className='mt-12 flex flex-wrap justify-evenly gap-x-6 gap-y-14'>
+                    {linguistic?.map((item, index) => (
+                        <div
+                            key={index}
+                            className='w-full max-w-[318px]'
+                        >
+                            <button className='p-[13px] rounded-full bg-greens-900'>
+                                <Image
+                                    src={item.img}
+                                    width={80}
+                                    height={80}
+                                    alt='Waves'
+                                />
+                            </button>
+                            <h4 className='mt-8 text-blues-1100 font-bold text-xl'>
+                                {item.name}
+                            </h4>
+                            <p className='mt-6 text-grays-800 text-xl'>
+                                {item.description}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ app/components/cultural-resources/VideoLibrary.jsx
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Video = [
+    {
+        img: "/cultural-resources/video1.svg",
+        name: "Cultural Insights Unveiled",
+        text: `Our Cultural Insights videos offer an immersive journey into the vibrant stories, and customs that define Nigerian culture`
+    },
+    {
+        img: "/cultural-resources/video2.svg",
+        name: "Taste of Nigeria: Culinary Delights",
+        text: `Feast your eyes on delectable dishes, and learn the art of preparing Nigerian delicacies`
+    },
+    {
+        img: "/cultural-resources/video3.svg",
+        name: "Celebrate the Diversity of Culture",
+        text: `Discover the colors, dances, and traditions that mark these joyous occasions`
+    },
+]
+
+export default function VideoLibrary() {
+    return (
+        <section className='mt-[86px] font-arimo px-5'>
+            <div className='container mx-auto bg-greens-1100 pt-16 pb-[118px]'>
+                <h1 className='text-center text-greens-200 font-bold text-5xl leading-normal'>
+                    Video Library
+                </h1>
+                <div className='mt-12 flex flex-wrap justify-evenly gap-x-6 gap-y-14'>
+                    {Video?.map((item, index) => (
+                        <div
+                            key={index}
+                            className='w-full max-w-[382px]'
+                        >
+                            <div>
+                                <Image
+                                    src={item.img}
+                                    width={1880}
+                                    height={1254}
+                                />
+                            </div>
+                            <h4 className='mt-8 font-bold text-xl text-grays-1000'>
+                                {item.name}
+                            </h4>
+                            <p className='mt-6 text-grays-800 text-xl'>
+                                {item.text}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
