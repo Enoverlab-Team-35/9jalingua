b535b5fd13402117ee2ff73b943f3b9059ff3615
Toheeb Opeyemi AJAYI <toheebopeyemi9@gmail.com>
2023-12-08 21:50:19 +0100

    Fixed

--- app/layouts/DashboardLayout.jsx
+++ app/layouts/DashboardLayout.jsx
@@ -4,9 +4,9 @@ import DashboardNavbar from '../components/DashboardNavbar'
 
 export default function DashboardLayout({ children }) {
     return (
-        <div className='relative font-arimo flex'>
+        <div className='relative font-arimo flex h-screen overflow-hidden m-0 p-0'>
             <Sidebar />
-            <div className='relative w-full h-screen overflow-hidden'>
+            <div className='relative w-full h-screen overflow-hidden m-0 p-0'>
                 <DashboardNavbar />
                 <div className="mt-0 pb-20 px-5 lg:px-10 h-[calc(100vh-90px)] overflow-x-hidden overflow-y-auto">
                     {children}
